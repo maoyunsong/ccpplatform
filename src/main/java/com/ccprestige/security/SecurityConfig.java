594897930b0e08019aa4eb404724e14dea712ac7
yunsong <yunsong.mao@gmail.com>
2015-06-23 21:46:18 +0200

    init projet ccprestige platform

---
+++ src/main/java/com/ccprestige/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ccprestige.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGloabal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("mao").password("123456").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.and().formLogin()
+			.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));;
+	}
+
+}
